43abaa6374d13a2ab8e3ef41a87137f3a15b014f
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2017-09-15 15:33:36 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -327,7 +327,7 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
-	<script src='https://code.responsivevoice.org/responsivevoice.js'></script>
+	
 	responsiveVoice.speak("Hello World");
 	}
 
